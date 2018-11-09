29293963ae030a46e8294212608dd9e00ab795c4
william <2601865991@qq.com>
2018-11-09 13:51:31 +0800

    Initial commit

---
+++ src/main/java/com/wl/dw/DwApplication.java
@@ -0,0 +1,12 @@
+package com.wl.dw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwApplication.class, args);
+    }
+}
